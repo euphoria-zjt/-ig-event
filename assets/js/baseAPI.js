f40ed299ddeb16a14101edc647b6fa54d941770d
anthony <sunshine-8886@qq.com>
2022-05-13 23:58:39 +0800

    完成个人中心功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,7 +4,7 @@
 $.ajaxPrefilter(function (options) {
   // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
   options.url = "http://www.liulongbin.top:3007" + options.url;
-  console.log(options.url);
+  // console.log(options.url);
   if (options.url.indexOf("/my/") !== -1) {
     options.headers = {
       Authorization: localStorage.getItem("token") || "",
